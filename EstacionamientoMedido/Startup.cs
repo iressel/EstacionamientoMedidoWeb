3ef7c045293c760af7a01481c4529d0008e4584a
Bnejmainlion <usuario@DESKTOP-NAPC569>
2022-11-01 21:02:04 -0300

    Cambios de formulario y plantillas.

--- EstacionamientoMedido/Startup.cs
+++ EstacionamientoMedido/Startup.cs
@@ -62,7 +62,7 @@ namespace EstacionamientoMedido
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Tickets}/{action=Create}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
